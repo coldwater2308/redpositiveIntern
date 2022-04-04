abfab91f6d160c86dfe062637d660a6343a76fff
coldwater2308 <82825896+coldwater2308@users.noreply.github.com>
2022-04-04 15:53:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const mongoose = require("mongoose");
 
 app.use(express.json());
 
- app.use('/api/data',dataRoutes);
+app.use('/api/data',dataRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 
 const PORT = process.env.PORT||5000;
